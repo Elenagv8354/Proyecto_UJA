17a597851dbb0464536bb98e51a9f441cad0d726
Elenagv8354 <egv0010@alu.medac.es>
2025-04-01 14:40:53 +0200

    Activity de Bienvenida
    Terminada estructura, pendiente modificar botones y ajustes definitivos.

---
+++ app/src/main/java/com/example/walletssi/ActivityBienvenida.java
@@ -0,0 +1,14 @@
+package com.example.walletssi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityBienvenida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida);
+    }
+}
